2fcdccd0e783828df41a82ea619658497b642fa3
ustotsu <32339438+ootsumasaya@users.noreply.github.com>
2022-10-29 21:21:26 +0900

    データベースの接続．autherが何を返しているのかテスト

--- discordbot.py
+++ discordbot.py
@@ -2,12 +2,22 @@ import discord
 import traceback
 from discord.ext import commands
 from os import getenv
+import os
+import psycopg2
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = commands.Bot(command_prefix='/', intents=intents)
-member_list = []
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
+cur = conn.cursor()
+
+cur.execute("select * from kaikei")
+results = cur.fetchall()
+member_payment_dict = {}
+for (member, payment) in results:
+    member_payment_dict[member] = payment
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -15,20 +25,32 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
-async def add_member(ctx, arg):
+async def add(ctx, arg):
+    # member_payment_dict
+    # cur.execute(
     member_list.append(arg)
     await ctx.send(' '.join(member_list))
     
-
+@bot.command()
+async def test(ctx,arg):
+    await ctx.send(ctx.author)
+    
+@bot.command()
+async def read(ctx):
+    cur.execute("select * from kaikei")
+    results = cur.fetchall()
+    member_payment_dict = {}
+    for (member, payment) in results:
+        member_payment_dict[member] = payment
+        
 @bot.command()
 async def show(ctx):
-    await ctx.send(' '.join(member_list))
+    await ctx.send('\n'.join("{0}:{1}".format(member,payment) for (member,payment) in member_payment_dict.items()))
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
