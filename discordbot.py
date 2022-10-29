10035b3d793b80db4a4b4a8296a9cf96c610e389
ustotsu <32339438+ootsumasaya@users.noreply.github.com>
2022-10-29 20:10:24 +0900

    メンバーリストとadd,showの追加

--- discordbot.py
+++ discordbot.py
@@ -7,7 +7,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
-
+member_list = []
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -20,6 +20,15 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def add_user(ctx, arg):
+    member_list.add(arg)
+    await ctx.send(' '.join(member_list))
+    
+
+@bot.command()
+async def show(ctx, arg):
+    await ctx.send(' '.join(member_list))
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
